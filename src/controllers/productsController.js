6983340cd7214c64b1f82dced6c4dc3688500124
Amanda Portela <mandaptela@gmail.com>
2023-02-14 23:37:57 -0300

    erro

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -1,1 +1,19 @@
-// Iniciando
+// Iniciandoconst productsService = require('../services/productsService');
+
+const getAll = async (_request, response) => {
+/*   const products = await productsService.getAllService();
+  response.status(200).json(products) */
+  console.log('products');
+}
+
+/* const getById = (request, response) => {
+  const id = request.params;
+  console.log(id);
+  const productsById = getAllService(requisicao);
+  response.status(200).json(productsById)
+} */
+
+module.exports = {
+  getAll,
+  //getById,
+}
---
+++ src/models/productsModel.js
@@ -0,0 +1,10 @@
+const { connection } = require("./connection");
+
+const getAllModel = async () => {
+  const query = 'SELECT * FROM StoreManager.products';
+  const result = await connection.execute(query);
+  console.log(result);
+  return result;
+}
+
+module.exports = { getAllModel }
