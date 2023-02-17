3e0b6eabc6d25c16fb171da1ba078a18b70966c0
Amanda Portela <mandaptela@gmail.com>
2023-02-17 17:37:19 -0300

    Requisito 6 - update

--- src/controllers/salesController.js
+++ src/controllers/salesController.js
@@ -1,13 +1,15 @@
 const salesService = require('../services/saleService');
 
-const addSale = async (request, response, next) => {
+const addSale = async (request, response) => {
   // const allSales = await salesService.addSaleService();
   const newSale = request.body;
   try {
     await salesService.addSaleRegistry()
     const result = await salesService.addSaleService(newSale);
     response.status(201).json(result);
-  } catch (error) { next(error); }
+  } catch (error) {
+    response.status(error.status).json({ message: error.message });
+  }
 };
 
 module.exports = { addSale };
--- src/services/saleService.js
+++ src/services/saleService.js
@@ -2,15 +2,6 @@ const Joi = require('joi');
 const salesModel = require('../models/saleModel');
 const productsModel = require('../models/productsModel');
 
-const nameSchema =  Joi.object({
-  productId: Joi.number().integer().min(1).required().label('productId'),
-  quantity: Joi.number().integer().min(1).required().label('quantity'),
-}).messages({
-  'any.required': '{{#label}} is required',
-  'number.min': '{{#label}} must be greater than or equal to 1',
-  'number.empty': '{{#label} Product not found',
-});
-
 const addSaleRegistry = async () => {
   const result = await salesModel.addSaleModel();
   return result;
@@ -18,13 +9,24 @@ const addSaleRegistry = async () => {
 
 const addSaleService = async (sale) => {
   const allSales = await salesModel.getSalesModel();
-  // const allProducts = await productsModel.getAllModel();
-
+  const allProducts = await productsModel.getAllModel();
+  const nameSchema =  Joi.object({
+    productId: Joi.number().integer().min(1).max(allProducts.length).required().label('productId'),
+    quantity: Joi.number().integer().min(1).required().label('quantity'),
+  }).messages({
+    'any.required': '{{#label}} is required',
+    'number.min': '{{#label}} must be greater than or equal to 1',
+    'number.max': '{{#label} Product not found',
+  });
 
   const arraySchema = Joi.array().items(nameSchema);
   const { error } = arraySchema.validate(sale);
-  if (error) throw { status: 400, message: error.message }
-
+  console.log(error);
+  if (error) {
+    if (error.type === 'any.required') throw { status: 400, message: error.message };
+    if (error.type === 'number.min') throw { status: 422, message: error.message };
+    if (error.type === 'number.max') throw { status: 404, message: error.message };
+  }
   await Promise.all(allSales.map(async (sales) => { await salesModel.addCompleteSaleModel(sales.id, sale); }));
   const ultimo =allSales[allSales.length - 1].id;
   console.log(ultimo);
